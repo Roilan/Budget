0e119cad32069084c3b4cb2d136cb86b478dc45f
Roilan <roilan14@gmail.com>
2015-02-05 18:40:58 -0500

    push expense names into graph labels

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,17 +10,50 @@
 angular.module('budgetApp')
   .controller('MainCtrl', function($scope) {
 
-  	$scope.expenses = {
+  	$scope.expenseName = {
   		rent: '',
   		internet: '',
   		spotify: '',
   		hulu: ''
   	};
 
-  	$scope.displayExpenses = function() {
-  		console.log($scope.expenses.rent)
-  		console.log($scope.expenses.internet)
+  	$scope.addExpenseNames = function() {
+
+  		angular.forEach($scope.expenseName, function(value, key) {
+  			$scope.graphData.labels.push(key);
+  		});
+
   	};
+
+  	$scope.addExpensesAmounts = function() {
+  		// Push getData key:values into $scope.graphData.datasets[0].data
+
+
+  	};
+
+  	$scope.displayExpenses = function() {
+  		$scope.addExpenseNames();
+  		//console.log($scope.graphData.labels)
+  		//console.log($scope.graphData.datasets[0].data)
+  		//console.log(Object.keys($scope.getData))
+  	};
+
+  	$scope.graphData = {
+  		// Expense names get pushed into labels array
+  		labels: [],
+
+  		datasets: [
+  			{
+  				label: 'My First dataset',
+	            fillColor: "rgba(151,187,205,0.5)",
+	            strokeColor: "rgba(151,187,205,0.8)",
+	            highlightFill: "rgba(151,187,205,0.75)",
+	            highlightStroke: "rgba(151,187,205,1)",
+	            data: [] // Expense amounts get pushed into data array
+  			}
+  		]
+  	};
+
 
    //  $scope.myData = {
   	// 	labels: ['Jan', 'Feb'],
