a4ad0ae7622be69a4cf7ee249ce168ea8f1142b6
Roilan <roilan14@gmail.com>
2015-02-05 17:26:55 -0500

    html and angular

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,45 @@
  * Controller of the budgetApp
  */
 angular.module('budgetApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function($scope) {
+
+  	$scope.expenses = {
+  		rent: '',
+  		internet: '',
+  		spotify: '',
+  		hulu: ''
+  	};
+
+  	$scope.displayExpenses = function() {
+  		console.log($scope.expenses.rent)
+  		console.log($scope.expenses.internet)
+  	};
+
+   //  $scope.myData = {
+  	// 	labels: ['Jan', 'Feb'],
+
+  	// 	datasets: [
+	  //       {
+	  //           label: 'My First dataset',
+	  //           fillColor: 'rgba(220,220,220,0.2)',
+	  //           strokeColor: 'rgba(220,220,220,1)',
+	  //           pointColor: 'rgba(220,220,220,1)',
+	  //           pointStrokeColor: '#fff',
+	  //           pointHighlightFill: '#fff',
+	  //           pointHighlightStroke: 'rgba(220,220,220,1)',
+	  //           data: [65, 59]
+	  //       },
+	  //       {
+	  //           label: 'My Second dataset',
+	  //           fillColor: 'rgba(151,187,205,0.2)',
+	  //           strokeColor: 'rgba(151,187,205,1)',
+	  //           pointColor: 'rgba(151,187,205,1)',
+	  //           pointStrokeColor: '#fff',
+	  //           pointHighlightFill: '#fff',
+	  //           pointHighlightStroke: 'rgba(151,187,205,1)',
+	  //           data: [28, 48]
+	  //       }
+  	// 	]
+  	// };
+
   });
